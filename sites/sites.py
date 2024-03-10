da2fbb036ea831c0fed1f8eeac22ea79fa8985f8
jbuendia1y <jbuendia1y@gmail.com>
2024-03-10 09:55:47 -0500

    + Latina

--- sites/sites.py
+++ sites/sites.py
@@ -3,6 +3,7 @@ from sites.rpp.RPP import RPP
 from sites.canal_n.CanalN import CanalN
 from sites.el_peruano.ElPeruano import ElPeruano
 from sites.peru21.Peru21 import Peru21
+from sites.latina.Latina import Latina
 
 import asyncio
 
@@ -14,6 +15,7 @@ async def run():
         RPP().run(),
         CanalN().run(),
         ElPeruano().run(),
-        Peru21().run()
+        Peru21().run(),
+        Latina().run()
     ])
     print("Sites scraping ends")
